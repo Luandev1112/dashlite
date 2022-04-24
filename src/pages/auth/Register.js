54087ef104ff15fd17aab916d502c3b1ec3da194
Dmitrii923 <joaosouto985@gmail.com>
2022-04-24 01:37:14 +0000

    made register page design

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
-import Logo from "../../images/logo.png";
+import Logo from "../../images/logo_white.svg";
 import LogoDark from "../../images/logo-dark.png";
 import PageContainer from "../../layout/page-container/PageContainer";
 import Head from "../../layout/head/Head";
-import AuthFooter from "./AuthFooter";
 import {
   Block,
   BlockContent,
@@ -13,151 +12,152 @@ import {
   Button,
   Icon,
   PreviewCard,
+  Row,
+  Col,
 } from "../../components/Component";
-import { Spinner, FormGroup } from "reactstrap";
+import { Form, FormGroup, Spinner, Alert } from "reactstrap";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
-const Register = ({ history }) => {
-  const [passState, setPassState] = useState(false);
+const Register = () => {
   const [loading, setLoading] = useState(false);
+  const [passState, setPassState] = useState(false);
+  const [errorVal, setError] = useState("");
+
+  const onFormSubmit = (formData) => {
+    setLoading(true);
+    const loginName = "info@softnio.com";
+    const pass = "123456";
+    if (formData.name === loginName && formData.passcode === pass) {
+      localStorage.setItem("accessToken", "token");
+      setTimeout(() => {
+        window.history.pushState(
+          `${process.env.PUBLIC_URL ? process.env.PUBLIC_URL : "/"}`,
+          "auth-login",
+          `${process.env.PUBLIC_URL ? process.env.PUBLIC_URL : "/"}`
+        );
+        window.location.reload();
+      }, 2000);
+    } else {
+      setTimeout(() => {
+        setError("Cannot login with credentials");
+        setLoading(false);
+      }, 2000);
+    }
+  };
+
   const { errors, register, handleSubmit } = useForm();
 
-  const handleFormSubmit = () => {
-    setLoading(true);
-    setTimeout(() => history.push(`${process.env.PUBLIC_URL}/auth-success`), 2000);
-  };
   return (
     <React.Fragment>
-      <Head title="Register" />
+      <Head title="Login" />
       <PageContainer>
-        <Block className="nk-block-middle nk-auth-body  wide-xs">
-          <div className="brand-logo pb-4 text-center">
-            <Link to={`${process.env.PUBLIC_URL}/`} className="logo-link">
-              <img className="logo-light logo-img logo-img-lg" src={Logo} alt="logo" />
-              <img className="logo-dark logo-img logo-img-lg" src={LogoDark} alt="logo-dark" />
-            </Link>
-          </div>
-          <PreviewCard className="card-bordered" bodyClass="card-inner-lg">
-            <BlockHead>
-              <BlockContent>
-                <BlockTitle tag="h4">Register</BlockTitle>
-                <BlockDes>
-                  <p>Create New Dashlite Account</p>
-                </BlockDes>
-              </BlockContent>
-            </BlockHead>
-            <form className="is-alter" onSubmit={handleSubmit(handleFormSubmit)}>
-              <FormGroup>
-                <label className="form-label" htmlFor="name">
-                  Name
-                </label>
-                <div className="form-control-wrap">
-                  <input
-                    type="text"
-                    id="name"
-                    name="name"
-                    placeholder="Enter your name"
-                    ref={register({ required: true })}
-                    className="form-control-lg form-control"
-                  />
-                  {errors.name && <p className="invalid">This field is required</p>}
-                </div>
-              </FormGroup>
-              <FormGroup>
-                <div className="form-label-group">
-                  <label className="form-label" htmlFor="default-01">
-                    Email or Username
-                  </label>
-                </div>
-                <div className="form-control-wrap">
-                  <input
-                    type="text"
-                    bssize="lg"
-                    id="default-01"
-                    name="email"
-                    ref={register({ required: true })}
-                    className="form-control-lg form-control"
-                    placeholder="Enter your email address or username"
-                  />
-                  {errors.email && <p className="invalid">This field is required</p>}
-                </div>
-              </FormGroup>
-              <FormGroup>
-                <div className="form-label-group">
-                  <label className="form-label" htmlFor="password">
-                    Passcode
-                  </label>
-                </div>
-                <div className="form-control-wrap">
-                  <a
-                    href="#password"
-                    onClick={(ev) => {
-                      ev.preventDefault();
-                      setPassState(!passState);
-                    }}
-                    className={`form-icon lg form-icon-right passcode-switch ${passState ? "is-hidden" : "is-shown"}`}
-                  >
-                    <Icon name="eye" className="passcode-icon icon-show"></Icon>
+        <Block className="nk-block-middle nk-auth-body">
+          <Row>
+            <Col lg="8" className="auth-col">
+              <Block className="text-center base-block" bodyClass="card-inner-lg">
+                <BlockHead>
+                  <BlockContent>
+                    <Link to={`${process.env.PUBLIC_URL}/`} className="logo-link">
+                      <img className="logo-dark logo-img" src={Logo} alt="logo" />
+                      <BlockTitle tag="h4">TrustBase</BlockTitle>
+                    </Link>
+                    <BlockTitle className="base-title" tag="h4">
+                      <i>Ease </i> and <i>security </i> at <br />
+                      your fingertips
+                    </BlockTitle>
+                    <BlockDes className="base-description">
+                      TrustBase enables you to securely and <br />
+                        easily borrow / lend money
+                    </BlockDes>
+                  </BlockContent>
+                </BlockHead>
+              </Block>
+            </Col>
+            <Col lg="4" className="auth-col">
+              <PreviewCard className="card-bordered" bodyClass="card-inner-lg">
+                <BlockHead>
+                  <BlockContent>
+                    <BlockTitle tag="h4">Register</BlockTitle>
+                    <BlockDes className="account">
+                      New Account
+                    </BlockDes>
+                  </BlockContent>
+                </BlockHead>
+                {errorVal && (
+                  <div className="mb-3">
+                    <Alert color="danger" className="alert-icon">
+                      {" "}
+                      <Icon name="alert-circle" /> Unable to login with credentials{" "}
+                    </Alert>
+                  </div>
+                )}
+                <Form className="is-alter" onSubmit={handleSubmit(onFormSubmit)}>
+                  <FormGroup>
+                    <div className="form-label-group">
+                      <label className="form-label" htmlFor="default-01">
+                        Email Address
+                      </label>
+                    </div>
+                    <div className="form-control-wrap">
+                      <input
+                        type="text"
+                        id="default-01"
+                        name="name"
+                        ref={register({ required: "This field is required" })}
+                        defaultValue="info@softnio.com"
+                        placeholder="Enter your email address or username"
+                        className="form-control-lg form-control"
+                      />
+                      {errors.name && <span className="invalid">{errors.name.message}</span>}
+                    </div>
+                  </FormGroup>
+                  <FormGroup>
+                    <div className="form-label-group">
+                      <label className="form-label" htmlFor="password">
+                        Password
+                      </label>
+                    </div>
+                    <div className="form-control-wrap">
+                      <a
+                        href="#password"
+                        onClick={(ev) => {
+                          ev.preventDefault();
+                          setPassState(!passState);
+                        }}
+                        className={`form-icon lg form-icon-right passcode-switch ${passState ? "is-hidden" : "is-shown"}`}
+                      >
+                        <Icon name="eye" className="passcode-icon icon-show"></Icon>
 
-                    <Icon name="eye-off" className="passcode-icon icon-hide"></Icon>
-                  </a>
-                  <input
-                    type={passState ? "text" : "password"}
-                    id="password"
-                    name="passcode"
-                    ref={register({ required: "This field is required" })}
-                    placeholder="Enter your passcode"
-                    className={`form-control-lg form-control ${passState ? "is-hidden" : "is-shown"}`}
-                  />
-                  {errors.passcode && <span className="invalid">{errors.passcode.message}</span>}
+                        <Icon name="eye-off" className="passcode-icon icon-hide"></Icon>
+                      </a>
+                      <input
+                        type={passState ? "text" : "password"}
+                        id="password"
+                        name="passcode"
+                        defaultValue="123456"
+                        ref={register({ required: "This field is required" })}
+                        placeholder="Enter your passcode"
+                        className={`form-control-lg form-control ${passState ? "is-hidden" : "is-shown"}`}
+                      />
+                      {errors.passcode && <span className="invalid">{errors.passcode.message}</span>}
+                    </div>
+                  </FormGroup>
+                  <FormGroup>
+                    <Button size="lg" className="btn-block" type="submit" color="success">
+                      {loading ? <Spinner size="sm" color="light" /> : "Register"}
+                    </Button>
+                  </FormGroup>
+                </Form>
+                <div className="form-note-s2 text-center pt-4">
+                  {" "}
+                  Aleady have an account? <Link className="link-success" to={`${process.env.PUBLIC_URL}/auth-login`}>Login</Link>
                 </div>
-              </FormGroup>
-              <FormGroup>
-                <Button type="submit" color="primary" size="lg" className="btn-block">
-                  {loading ? <Spinner size="sm" color="light" /> : "Register"}
-                </Button>
-              </FormGroup>
-            </form>
-            <div className="form-note-s2 text-center pt-4">
-              {" "}
-              Already have an account?{" "}
-              <Link to={`${process.env.PUBLIC_URL}/auth-login`}>
-                <strong>Sign in instead</strong>
-              </Link>
-            </div>
-            <div className="text-center pt-4 pb-3">
-              <h6 className="overline-title overline-title-sap">
-                <span>OR</span>
-              </h6>
-            </div>
-            <ul className="nav justify-center gx-8">
-              <li className="nav-item">
-                <a
-                  className="nav-link"
-                  href="#socials"
-                  onClick={(ev) => {
-                    ev.preventDefault();
-                  }}
-                >
-                  Facebook
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link"
-                  href="#socials"
-                  onClick={(ev) => {
-                    ev.preventDefault();
-                  }}
-                >
-                  Google
-                </a>
-              </li>
-            </ul>
-          </PreviewCard>
+              </PreviewCard>
+            </Col>
+          </Row>
+          
         </Block>
-        <AuthFooter />
       </PageContainer>
     </React.Fragment>
   );
